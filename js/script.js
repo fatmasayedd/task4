7fbe3a22455d7ac016550efaac6862bc6d534e3f
fatmasayedd <116314385+fatmasayedd@users.noreply.github.com>
2022-12-01 04:44:39 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+﻿var allProduct=document.querySelectorAll(" #addtocard , #productNum")
+// var card=document.querySelector(" #addtocard")
+var content=document.querySelector(" #productName")
+var btnShowPrice=document.querySelector("#showPrice")
+var totalPrice=document.querySelector(" #Price1")
+var totalProduct=document.querySelector("#productNum")
+var x=0
+allProduct.forEach(function(item){
+  
+    item.onclick=function(){
+       
+        content.innerHTML += totalProduct.textContent+" , "
+
+        if( content.innerHTML !=""){
+        btnShowPrice.style.display="block";
+        btnShowPrice.style.backgroundcolor="white";
+        btnShowPrice.style.color="black";
+    }
+    x += parseInt(totalProduct.getAttribute("price"))
+    btnShowPrice.onclick=function(){
+        totalPrice.innerHTML=x
+
+    }
+    } 
+   
+}
+)
+
+// btnShowPrice.onclick=function(){console.log(x)}
